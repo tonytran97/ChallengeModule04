a4de2e0620e999d36415db87a46e6e58acf9a78c
tonytran97 <tonytran_97@live.com>
2022-07-30 19:01:24 -0700

    generation of question sets 3,4,5

--- assets/javascript.js
+++ assets/javascript.js
@@ -23,6 +23,18 @@ var questionSeries = [
         answers2:
             ["quotes", "curly brackets", "parenthesis", "square brackets"]
 
+    },
+    {
+        question3: "Arrays in JavaScript can be used to store ___________.",
+        answers3: ["numbers and strings", "other arrays", "booleans", "all of the above"]
+    },
+    {
+        question4: "String values must be enclosed within ______ when being assigned to variables.",
+        answers4: ["commas", "curly brackets", "quotes", "parenthesis"]
+    },
+    {
+        question5: "A very useful too used during development and debugging for printing content to the debugger is:",
+        answers5: ["JavaScript", "terminal/bash", "for loops", "console log"]
     }
 ];
 
@@ -64,7 +76,38 @@ function nextQuestion2() {
     option2.innerHTML = questionSeries[1].answers2[1];
     option3.innerHTML = questionSeries[1].answers2[2];
     option4.innerHTML = questionSeries[1].answers2[3];
+    option1.addEventListener("click", nextQuestion3);
 }
+
+function nextQuestion3() {
+    resetButtons;
+    questionEl.innerHTML = questionSeries[2].question3;
+    option1.innerHTML = questionSeries[2].answers3[0];
+    option2.innerHTML = questionSeries[2].answers3[1];
+    option3.innerHTML = questionSeries[2].answers3[2];
+    option4.innerHTML = questionSeries[2].answers3[3];
+    option1.addEventListener("click", nextQuestion4);
+}
+
+function nextQuestion4() {
+    resetButtons;
+    questionEl.innerHTML = questionSeries[3].question4;
+    option1.innerHTML = questionSeries[3].answers4[0];
+    option2.innerHTML = questionSeries[3].answers4[1];
+    option3.innerHTML = questionSeries[3].answers4[2];
+    option4.innerHTML = questionSeries[3].answers4[3];
+    option1.addEventListener("click", nextQuestion5);
+}
+
+function nextQuestion5() {
+    resetButtons;
+    questionEl.innerHTML = questionSeries[4].question5;
+    option1.innerHTML = questionSeries[4].answers5[0];
+    option2.innerHTML = questionSeries[4].answers5[1];
+    option3.innerHTML = questionSeries[4].answers5[2];
+    option4.innerHTML = questionSeries[4].answers5[3];
+}
+
 
 // Button test
 // document.getElementById("button").addEventListener("click", function () {
