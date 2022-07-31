8de5abbf4e196beb88ddd27222abf971082b472c
tonytran97 <tonytran_97@live.com>
2022-07-30 18:45:26 -0700

    generation of first & second set of questions

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,48 +1,72 @@
 const startButton = document.getElementById("button");
 var timer = document.getElementById("timer");
+var secondsLeft = 300;
 var hidden = document.getElementById("main");
 var questionSetContainer = document.querySelector("section");
-var secondsLeft = 300;
+var questionEl = document.getElementById("question");
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var option3 = document.getElementById("option3");
+var option4 = document.getElementById("option4");
 
 startButton.addEventListener("click", startGame);
 
-var Quiz = [
+var questionSeries = [
     {
-        question: "This is question 1",
-        answers: {
-            1: "This is choice 1",
-            2: "This is choice 2",
-            3: "This is choice 3",
-            4: "This is choice 4"
-        }
-    }
-]
+        question1: "Commonly used data types DO Not include: ",
+        answers1:
+            ["string", "booleans", "alerts", "numbers"]
 
-function startGame() {
+    },
+    {
+        question2: "The condition in an if/else statement is enclosed with ________",
+        answers2:
+            ["quotes", "curly brackets", "parenthesis", "square brackets"]
+
+    }
+];
+
+function startGame(event) {
+    event.preventDefault();
     var timeInterval = setInterval(function () {
         secondsLeft--;
         timer.innerHTML = "Time: " + secondsLeft;
-
         if (secondsLeft === 0) {
             clearInterval(timeInterval);
         }
-
     }, 1000);
     hidden.classList.add("hide");
     questionSetContainer.classList.remove("hidden");
-
+    nextQuestion();
 }
+
+function nextQuestion() {
+    questionEl.innerHTML = questionSeries[0].question1;
+    option1.innerHTML = questionSeries[0].answers1[0];
+    option2.innerHTML = questionSeries[0].answers1[1];
+    option3.innerHTML = questionSeries[0].answers1[2];
+    option4.innerHTML = questionSeries[0].answers1[3];
+    option1.addEventListener("click", nextQuestion2);
+}
+
+function resetButtons() {
+    questionEl.innerHTML = "";
+    option1.innerHTML = "";
+    option2.innerHTML = "";
+    option3.innerHTML = "";
+    option4.innerHTML = "";
+}
+
+function nextQuestion2() {
+    resetButtons;
+    questionEl.innerHTML = questionSeries[1].question2;
+    option1.innerHTML = questionSeries[1].answers2[0];
+    option2.innerHTML = questionSeries[1].answers2[1];
+    option3.innerHTML = questionSeries[1].answers2[2];
+    option4.innerHTML = questionSeries[1].answers2[3];
+}
+
 // Button test
 // document.getElementById("button").addEventListener("click", function () {
 //     prompt("hello");
 // });
-
-// function disappear() {
-//     hidden.style.display = "none";
-//     const h1 = document.createElement("h1");
-//     h1.innerText = Quiz.question;
-//     document.body.appendChild(h1);
-//     const listedAnswers = document.createElement("ol");
-//     listedAnswers.innerText = Quiz.answers;
-//     document.body.appendChild(listedAnswers);
-// }
