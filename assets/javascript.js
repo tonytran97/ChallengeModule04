8935779dd5d5ab9e51547ad7f25a92b5102a5f20
tonytran97 <tonytran_97@live.com>
2022-08-01 01:17:10 -0700

    functioning next question features to GameOver

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,4 @@
-const startButton = document.getElementById("button");
+const startButton = document.getElementById("quizButton");
 var timer = document.getElementById("timer");
 var secondsLeft = 300;
 var hidden = document.getElementById("main");
@@ -8,33 +8,38 @@ var option1 = document.getElementById("option1");
 var option2 = document.getElementById("option2");
 var option3 = document.getElementById("option3");
 var option4 = document.getElementById("option4");
+var answerButtons = document.getElementById("answer-choices");
+var preClear = document.querySelector("pre");
+var h1 = document.querySelector("h1");
+var h2 = document.querySelector("h2");
 
 startButton.addEventListener("click", startGame);
 
 var questionSeries = [
     {
-        question1: "Commonly used data types DO Not include: ",
-        answers1:
-            ["string", "booleans", "alerts", "numbers"]
-
+        question: "Commonly used data types DO Not include: ",
+        choices: ["string", "booleans", "alerts", "numbers"],
+        correct: "alerts",
     },
     {
-        question2: "The condition in an if/else statement is enclosed with ________",
-        answers2:
-            ["quotes", "curly brackets", "parenthesis", "square brackets"]
-
+        question: "The condition in an if/else statement is enclosed with ________",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correct: "parenthesis"
     },
     {
-        question3: "Arrays in JavaScript can be used to store ___________.",
-        answers3: ["numbers and strings", "other arrays", "booleans", "all of the above"]
+        question: "Arrays in JavaScript can be used to store ___________.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct: "all of the above"
     },
     {
-        question4: "String values must be enclosed within ______ when being assigned to variables.",
-        answers4: ["commas", "curly brackets", "quotes", "parenthesis"]
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correct: "quotes"
     },
     {
-        question5: "A very useful too used during development and debugging for printing content to the debugger is:",
-        answers5: ["JavaScript", "terminal/bash", "for loops", "console log"]
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
+        correct: "console log"
     }
 ];
 
@@ -49,16 +54,20 @@ function startGame(event) {
     }, 1000);
     hidden.classList.add("hide");
     questionSetContainer.classList.remove("hidden");
-    nextQuestion();
+    currentQuestion = 0
+    showQuestion();
 }
 
-function nextQuestion() {
-    questionEl.innerHTML = questionSeries[0].question1;
-    option1.innerHTML = questionSeries[0].answers1[0];
-    option2.innerHTML = questionSeries[0].answers1[1];
-    option3.innerHTML = questionSeries[0].answers1[2];
-    option4.innerHTML = questionSeries[0].answers1[3];
-    option1.addEventListener("click", nextQuestion2);
+function showQuestion() {
+    questionEl.innerHTML = questionSeries[currentQuestion].question;
+    option1.innerHTML = questionSeries[currentQuestion].choices[0];
+    option2.innerHTML = questionSeries[currentQuestion].choices[1];
+    option3.innerHTML = questionSeries[currentQuestion].choices[2];
+    option4.innerHTML = questionSeries[currentQuestion].choices[3];
+    option1.addEventListener("click", answerChoice);
+    option2.addEventListener("click", answerChoice);
+    option3.addEventListener("click", answerChoice);
+    option4.addEventListener("click", answerChoice);
 }
 
 function resetButtons() {
@@ -69,47 +78,46 @@ function resetButtons() {
     option4.innerHTML = "";
 }
 
-function nextQuestion2() {
-    resetButtons;
-    questionEl.innerHTML = questionSeries[1].question2;
-    option1.innerHTML = questionSeries[1].answers2[0];
-    option2.innerHTML = questionSeries[1].answers2[1];
-    option3.innerHTML = questionSeries[1].answers2[2];
-    option4.innerHTML = questionSeries[1].answers2[3];
-    option1.addEventListener("click", nextQuestion3);
+function answerChoice(event) {
+    var selectedAnswer = event.target.innerHTML;
+    console.log(currentQuestion);
+    if (questionSeries[currentQuestion].correct === selectedAnswer) {
+
+        console.log("+10");
+    } else {
+        console.log("-10");
+    }
+    resetButtons();
+    currentQuestion++;
+    if (currentQuestion < questionSeries.length) {
+        nextQuestion();
+    } else {
+        gameOver();
+    }
 }
 
-function nextQuestion3() {
-    resetButtons;
-    questionEl.innerHTML = questionSeries[2].question3;
-    option1.innerHTML = questionSeries[2].answers3[0];
-    option2.innerHTML = questionSeries[2].answers3[1];
-    option3.innerHTML = questionSeries[2].answers3[2];
-    option4.innerHTML = questionSeries[2].answers3[3];
-    option1.addEventListener("click", nextQuestion4);
+function nextQuestion() {
+    questionEl.innerHTML = questionSeries[currentQuestion].question;
+    option1.innerHTML = questionSeries[currentQuestion].choices[0];
+    option2.innerHTML = questionSeries[currentQuestion].choices[1];
+    option3.innerHTML = questionSeries[currentQuestion].choices[2];
+    option4.innerHTML = questionSeries[currentQuestion].choices[3];
+    option1.addEventListener("click", answerChoice);
+    option2.addEventListener("click", answerChoice);
+    option3.addEventListener("click", answerChoice);
+    option4.addEventListener("click", answerChoice);
 }
 
-function nextQuestion4() {
-    resetButtons;
-    questionEl.innerHTML = questionSeries[3].question4;
-    option1.innerHTML = questionSeries[3].answers4[0];
-    option2.innerHTML = questionSeries[3].answers4[1];
-    option3.innerHTML = questionSeries[3].answers4[2];
-    option4.innerHTML = questionSeries[3].answers4[3];
-    option1.addEventListener("click", nextQuestion5);
+function gameOver() {
+    questionSetContainer.classList.add("hidden");
+    hidden.classList.remove("hide");
+    preClear.classList.add("hidden");
+    h1.remove();
+    h2.remove();
+    startButton.remove();
+    var gameOverBox = document.createElement("div");
+    var gameOverText = document.createTextNode("GameOver!");
+    hidden.appendChild(gameOverBox);
+    gameOverBox.appendChild(gameOverText);
+    gameOverBox.style.fontSize = "5vh";
 }
-
-function nextQuestion5() {
-    resetButtons;
-    questionEl.innerHTML = questionSeries[4].question5;
-    option1.innerHTML = questionSeries[4].answers5[0];
-    option2.innerHTML = questionSeries[4].answers5[1];
-    option3.innerHTML = questionSeries[4].answers5[2];
-    option4.innerHTML = questionSeries[4].answers5[3];
-}
-
-
-// Button test
-// document.getElementById("button").addEventListener("click", function () {
-//     prompt("hello");
-// });
